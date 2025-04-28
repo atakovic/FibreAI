961da96829a84779b0829bfd2820e88e15a84325
atakovic <93774866+atakovic@users.noreply.github.com>
2025-04-28 23:37:50 +0200

    Update 6_Kontakt.py

--- pages/6_Kontakt.py
+++ pages/6_Kontakt.py
@@ -17,7 +17,7 @@ st.write("Falls es Komplikationen gibt oder es zu Rückfragen kommen sollte, kö
 
 kontakt2, kontakt3 = st.columns(2)
 with kontakt2:
-    st.image("/opt/lampp/htdocs/Webseite_SHK/streamlit/webpictures/beer.png", width=image_width)
+    st.image("webpictures/beer.png", width=image_width)
     st.write("**Prof. Mathias Beer**")
     st.write("Professor der Lehrveranstaltung")
     st.write("Email: mathias.beer@hs-niederrhein.de")
@@ -25,7 +25,7 @@ with kontakt2:
     st.write("Webschulstraße 8")
     st.write("41065 Mönchengladbach")
 with kontakt3:
-    st.image("/opt/lampp/htdocs/Webseite_SHK/streamlit/webpictures/hellweg.png", width=image_width)
+    st.image("webpictures/hellweg.png", width=image_width)
     st.write("**Lennart Hellweg**")
     st.write("Dozent der Lehrveranstaltung")
     st.write("Email: lennart.hellweg@hs-niederrhein.de")
@@ -35,17 +35,17 @@ with kontakt3:
 
 kontakt1, kontakt4, kontakt5 = st.columns(3)
 with kontakt1:
-    st.image("/opt/lampp/htdocs/Webseite_SHK/streamlit/webpictures/tabak.png", width=image_width)
+    st.image("webpictures/tabak.png", width=image_width)
     st.write("**Alen Tabakovic**")
     st.write("Technischer Support")
     st.write("Email: alen.tabakovic@stud.hn.de")
 with kontakt4:
-    st.image("/opt/lampp/htdocs/Webseite_SHK/streamlit/webpictures/radau.png", width=image_width)
+    st.image("webpictures/radau.png", width=image_width)
     st.write("**Natalie Radau**")
     st.write("Projekt Support")
     st.write("Email: natalie.radau@stud.hn.de")
 with kontakt5:
-    st.image("/opt/lampp/htdocs/Webseite_SHK/streamlit/webpictures/brodka.png", width=image_width)
+    st.image("webpictures/brodka.png", width=image_width)
     st.write("**Lisa-Marie Brodka**")
     st.write("Projekt Support")
     st.write("Email: lisa-marie.brodka@stud.hn.de")
@@ -53,7 +53,7 @@ with kontakt5:
 #-------------------------------------------------------------------------------------------
 ### Seitenleiste
 # Öffne das Bild
-image = Image.open("/opt/lampp/htdocs/Webseite_SHK/streamlit/webpictures/fibreai.png")
+image = Image.open("webpictures/fibreai.png")
 
 # Konvertiere das Bild in Base64
 buffer = BytesIO()
@@ -63,6 +63,6 @@ data = base64.b64encode(buffer.read()).decode("utf-8")
 
 # Benutzerdefiniertes HTML mit Base64-Bild
 #st.sidebar.header("FibreAI")
-image = Image.open("/opt/lampp/htdocs/Webseite_SHK/streamlit/webpictures/fibreai.png")
+image = Image.open("webpictures/fibreai.png")
 st.sidebar.image(image)
 
